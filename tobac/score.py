5bd82d154ef01fb731fc0cd0e58f582bd7765145
Fabian <97912636+fziegner@users.noreply.github.com>
2023-12-04 15:26:29 +0100

    Update score.py

--- tobac/score.py
+++ tobac/score.py
@@ -1,10 +1,42 @@
-from pandas import *
-from numpy import *
+# bad_code.py
+import os, sys
 
-print(1)
-print(2)
-print(3)
+# Example of bad practices for Pylint
+class myClass:
+    def __init__(self,value=2):
+        self.value=value
+    
+    def BADMethodNaming(self, aValue):
+        try:
+            self.value/=aValue
+        except Exception, e:
+            print "Error detected:",e
+            exit(1)
+        return self.value
 
-a=1
-b=2
-c=3
+     def anothermethod(self): # Indentation inconsistency
+    pass
+
+def global_func():
+    global some_global
+    some_global = 1
+    some_global+=1
+
+# Unused variable and wildcard import
+from math import *
+
+# Unused argument
+def unused_arg_function(unused_arg):
+    return "Nothing"
+
+# Redefinition of a built-in
+list = ['a', 'b', 'c']
+
+class AnotherClass(object):
+  def not_used_method(self):
+      pass
+
+# usage of myClass that will increase the Pylint issues count
+instance = myClass()
+instance.BADMethodNaming(42)
+print(instance.BADMethodNaming('1'))
