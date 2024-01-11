0fdfd3699f4620de8312191c31a7bd541ac39daa
fziegner <Fabian.Ziegner@gmx.de>
2024-01-11 13:27:53 +0100

    Added NetCDF-based comparison of notebooks

--- realcase_testing.py
+++ realcase_testing.py
@@ -1,16 +1,19 @@
-import json
-import difflib
+import glob
 import os
+import xarray as xr
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+from nbconvert.preprocessors import CellExecutionError
+from git import Repo
+import tempfile
+
+# Configuration
+git_repo_path = "./"
+notebook_folder_name = "examples"
+reference_folder_name = "reference_data"
 
 
-def load_notebook(path: str) -> list:
-    with open(path, "r", encoding="utf-8") as file:
-        notebook = json.load(file)
-    filtered_notebook = [cell for cell in notebook["cells"] if cell.get("cell_type") == "code"]
-    return filtered_notebook
-
-
-def get_notebooks_paths(root_dir: str) -> dict:
+def get_notebooks_paths(root_dir):
     notebook_paths = {}
     for path, dirs, files in os.walk(root_dir):
         for file in files:
@@ -20,55 +23,86 @@ def get_notebooks_paths(root_dir: str) -> dict:
     return notebook_paths
 
 
-def get_outputs_from_cells(cells: list) -> list[dict]:
-    outputs = []
-    for cell in cells:
-        if cell["cell_type"] == "code":
-            cell_outputs = cell.get("outputs", [])
-            for output in cell_outputs:
-                match = {}
-                if 'data' in output and isinstance(output['data'], dict):
-                    for key, value in output['data'].items():
-                        if (key.startswith('text') and 'html' not in key) or key.startswith('image'):
-                            match[key] = value
-                else:
-                    for key, value in output.items():
-                        if (key.startswith('text') and 'html' not in key) or key.startswith('image'):
-                            match[key] = value
-                if match:
-                    outputs.append(match)
-    return outputs
+def run_notebook(notebook_path, output):
+
+    with open(notebook_path, 'r', encoding='utf-8') as f:
+        nb = nbformat.read(f, as_version=4)
+
+    ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+
+    try:
+        print(f"Running notebook {notebook_path}")
+        ep.preprocess(nb, {'metadata': {'path': output}})
+        print(f"Notebook {notebook_path} executed successfully!")
+    except CellExecutionError as e:
+        msg = f"Error executing the notebook {notebook_path}.\n"
+        msg += f'See the following error: {e}\n'
+        print(msg)
+        raise
 
 
-def compare_outputs(outputs1: list[dict], outputs2: list[dict], notebook_name: str) -> None:
-    diff = difflib.unified_diff(str(outputs1), str(outputs2), fromfile=f"Version1/{notebook_name}", tofile=f"Version2/{notebook_name}")
-    has_difference = False
-    for line in diff:
-        has_difference = True
-        print(line)
-    if not has_difference:
-        print(f"No differences found in outputs of notebook {notebook_name}")
+def compare_files(local_f, reference_f):
+
+    with xr.open_dataset(local_f) as ds_local, xr.open_dataset(reference_f) as ds_reference:
+        comparison_result = ds_local.equals(ds_reference)
+
+    return comparison_result
 
 
-version1_root = "./examples1"
-version2_root = "./examples2"
+def get_head_notebooks(repo_path):
 
-version1_notebooks = get_notebooks_paths(version1_root)
-version2_notebooks = get_notebooks_paths(version2_root)
+    repo = Repo(repo_path)
 
-# Iterate through one set of notebooks and find the matching counterpart
-for notebook1_name, notebook1_path in version1_notebooks.items():
-    notebook2_path = version2_notebooks.get(notebook1_name)
+    head_tree = repo.head.commit.tree
+    notebook_paths = [blob.path for blob in head_tree.traverse() if blob.path.startswith("examples") and blob.path.endswith(".ipynb")]
+    return notebook_paths
 
-    if notebook2_path is None:
-        print(f"Notebook {notebook1_name} does not exist in Version 2")
-        continue
 
-    notebook1_cells = load_notebook(notebook1_path)
-    notebook2_cells = load_notebook(notebook2_path)
+def read_file_in_head(repo_path, file_path):
 
-    notebook1_outputs = get_outputs_from_cells(notebook1_cells)
-    notebook2_outputs = get_outputs_from_cells(notebook2_cells)
+    repo = Repo(repo_path)
 
-    print(f"Comparing notebook {notebook1_name} outputs")
-    compare_outputs(notebook1_outputs, notebook2_outputs, notebook1_name)
+    file_blob = repo.head.commit.tree / file_path
+    file_content = file_blob.data_stream.read().decode('utf-8')
+
+    return file_content
+
+
+def main(source="wd", target="HEAD"):
+
+    with tempfile.TemporaryDirectory() as tmp:
+        if source == "HEAD":
+            notebooks = get_head_notebooks(git_repo_path)
+        else:
+            notebooks = get_notebooks_paths(git_repo_path + notebook_folder_name).values()
+
+        if target == "HEAD":
+            notebooks_reference = get_head_notebooks(git_repo_path)
+
+        for nb in notebooks:
+            notebook_name = nb.replace("\\", "/").split("/")[-1].split(".")[0]
+            os.mkdir(os.path.join(tmp, notebook_name))
+            run_notebook(nb, os.path.join(tmp, notebook_name))
+            netcdf_files = glob.glob(os.path.join(tmp, notebook_name, "Save", "*"))
+            if target == "zenodo":
+                reference_folder = git_repo_path + os.path.join(reference_folder_name, notebook_name, "Save")
+                for local_file in netcdf_files:
+                    reference_file = os.path.join(reference_folder, local_file.rsplit(os.sep, 1)[1])
+                    if os.path.exists(reference_file):
+                        result = compare_files(local_file, reference_file)
+                        print(f"Comparison result for {nb}: {'Same' if result else 'Different'}")
+            else:
+                for nb_ in notebooks_reference:
+                    if notebook_name in nb_:
+                        os.makedirs(os.path.join(tmp, notebook_name + "_reference"))
+                        run_notebook(nb_, os.path.join(tmp, notebook_name + "_reference"))
+                        netcdf_files_reference = glob.glob(os.path.join(tmp, notebook_name + "_reference", "Save", "*"))
+                        for local_file, reference_file in zip(netcdf_files, netcdf_files_reference):
+                            if os.path.exists(local_file) and os.path.exists(reference_file):
+                                result = compare_files(local_file, reference_file)
+                                print(f"Comparison result for {nb_}: {'Same' if result else 'Different'}")
+                        break
+
+
+if __name__ == "__main__":
+    main()
