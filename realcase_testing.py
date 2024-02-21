fb69c892cfd7a1cc1c5ef00ec99b2afcadde60d7
fziegner <Fabian.Ziegner@gmx.de>
2024-02-21 12:56:25 +0100

    Refactoring and fixed environments

--- realcase_testing.py
+++ realcase_testing.py
@@ -1,108 +1,22 @@
+import argparse
 import glob
 import os
 import subprocess
-import sys
+import tempfile
 
 import xarray as xr
-import nbformat
-from nbconvert.preprocessors import ExecutePreprocessor
-from nbconvert.preprocessors import CellExecutionError
 from git import Repo
 
-import tempfile
-import venv
-
-# Configuration
-git_repo_path = "./"
-notebook_folder_name = "examples"
-reference_folder_name = "reference_data"
-
-
-def get_notebooks_paths(root_dir):
-    notebook_paths = []
-    for path, dirs, files in os.walk(root_dir):
-        for file in files:
-            if file.startswith("Example") and file.endswith(".ipynb"):
-                notebook_paths.append(os.path.join(path, file))
-    return notebook_paths
+parser = argparse.ArgumentParser()
+parser.add_argument("--nb", type=str)
+parser.add_argument("--c1", type=str)
+parser.add_argument("--c2", type=str)
+parser.add_argument("--sv", type=str)
+parser.add_argument("--c", type=bool)
+args = parser.parse_args()
 
 
-def run_notebook(notebook_path, output):
-
-    with open(notebook_path, 'r', encoding='utf-8') as f:
-        nb = nbformat.read(f, as_version=4)
-
-    ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
-
-    try:
-        print(f"Running notebook {notebook_path}")
-        ep.preprocess(nb, {'metadata': {'path': output}})
-        print(f"Notebook {notebook_path} executed successfully!")
-    except CellExecutionError as e:
-        msg = f"Error executing the notebook {notebook_path}.\n"
-        msg += f'See the following error: {e}\n'
-        print(msg)
-        raise
-
-
-def compare_files(local_f, reference_f):
-
-    with xr.open_dataset(local_f) as ds_local, xr.open_dataset(reference_f) as ds_reference:
-        comparison_result = ds_local.equals(ds_reference)
-
-    return comparison_result
-
-
-def get_head_notebooks(repo_path):
-
-    repo = Repo(repo_path)
-
-    head_tree = repo.head.commit.tree
-    notebook_paths = [blob.path for blob in head_tree.traverse() if blob.path.startswith("examples") and blob.path.endswith(".ipynb")]
-    print(notebook_paths)
-    return notebook_paths
-
-
-def read_file_in_head(repo_path, file_path):
-
-    repo = Repo(repo_path)
-
-    file_blob = repo.head.commit.tree / file_path
-    file_content = file_blob.data_stream.read().decode('utf-8')
-
-    return file_content
-
-
-def download_reference_files(commit_hash, output_directory, temp_folder_name):
-
-    username = "fziegner"
-    repo_url = f'https://github.com/{username}/tobac.git'
-
-    repo = Repo.clone_from(repo_url, os.path.join(output_directory, temp_folder_name), no_checkout=True)
-    repo.git.checkout(commit_hash)
-
-    return os.path.join(output_directory, temp_folder_name)
-
-
-def create_reference_data(source_directory, output_folder_name, output_directory, notebook_folder_name):
-
-    reference_list = []
-    if not isinstance(source_directory, list):
-        notebooks = get_notebooks_paths(os.path.join(source_directory, notebook_folder_name))
-    else:
-        notebooks = source_directory
-
-    for notebook in notebooks:
-        notebook_name = os.path.basename(notebook).split(".")[0]
-        os.makedirs(os.path.join(output_directory, output_folder_name, notebook_name))
-        run_notebook(notebook, os.path.join(output_directory, output_folder_name, notebook_name))
-        reference_list.extend(glob.glob(os.path.join(output_directory, output_folder_name, notebook_name, "Save", "*")))
-        break
-
-    return reference_list
-
-
-def download_tobac(dest_directory, dest_folder, tag="v1.5.1", ):
+def download_tobac(dest_directory, dest_folder, tag="v1.5.1"):
 
     repo_url = f'https://github.com/tobac-project/tobac.git'
 
@@ -112,54 +26,108 @@ def download_tobac(dest_directory, dest_folder, tag="v1.5.1", ):
     return os.path.join(dest_directory, dest_folder)
 
 
-def create_venv(env_dir):
+def create_environment(environment_directory, tobac_version):
 
-    venv.create(env_dir, with_pip=True)
-
-    if sys.platform == "win32":
-        python_exec = os.path.join(env_dir, 'Scripts', 'python')
-        pip_exec = os.path.join(env_dir, 'Scripts', 'pip')
-    else:
-        python_exec = os.path.join(env_dir, 'bin', 'python')
-        pip_exec = os.path.join(env_dir, 'bin', 'pip')
-
-    return python_exec, pip_exec
+    if tobac_version.startswith("v"):
+        tobac_version = tobac_version[1::]
+    subprocess.run(["mamba", "create", "-y", "-p", environment_directory, "python"], shell=True, check=True)
+    subprocess.run(["mamba", "install", "-y", "-c", "conda-forge", "-p", environment_directory, f"tobac={tobac_version}", "jupyter", "arm_pyart", "pytables", "ffmpeg", "gitpython", "nbformat", "nbconvert"], shell=True, check=True)
+    #try:
+    #    subprocess.run(["mamba", "install", "-y", "-c", "conda-forge", "-p", environment_directory, "--file", os.path.join(tobac_directory, 'requirements.txt')], check=True)
+    #except:
+    #    subprocess.run(["mamba", "install", "-y", "-c", "conda-forge", "-p", environment_directory, "--file", os.path.join(tobac_directory, 'conda-requirements.txt')], check=True)
 
 
-def install_requirements(pip_exec, project_dir):
+def get_reference_file_paths(root_dir):
 
-    subprocess.run([pip_exec, 'install', '-r', os.path.join(project_dir, 'requirements.txt')], check=True)
-    subprocess.run([pip_exec, 'install', project_dir], check=True)
+    file_paths = []
+
+    for dir_path, dir_names, file_names in os.walk(root_dir):
+        for dir_name in [d for d in dir_names if d.startswith("Example")]:
+            example_folder_path = os.path.join(dir_path, dir_name)
+            file_paths.extend(glob.glob(os.path.join(example_folder_path, "Save", "*")))
+    return file_paths
 
 
-def main(source="HEAD", target="wd", source_commit=True, target_commit=True, source_commit_hash="d3bca1a1dc5e34ec7d69f968e21a2c71520c545b", target_commit_hash="d3bca1a1dc5e34ec7d69f968e21a2c71520c545b"):
+def compare_files(reference_file1, reference_file2):
+
+    with xr.open_dataset(reference_file1) as ds_local, xr.open_dataset(reference_file2) as ds_reference:
+        comparison_result = ds_local.equals(ds_reference)
+
+    return comparison_result
+
+
+def compare_files_detailed(reference_file1, reference_file2):
+
+    with open("comparison_results.txt", "a+") as f:
+        with xr.open_dataset(reference_file1) as ds_source, xr.open_dataset(reference_file2) as ds_target:
+            if ds_source.equals(ds_target):
+                print(f"Comparison result for {reference_file1} and {reference_file2}: Same")
+                f.write(f"Comparison result for {reference_file1} and {reference_file2}: Same\n")
+            else:
+                print(f"Comparison result for {reference_file1} and {reference_file2}: Different")
+                f.write(f"Comparison result for {reference_file1} and {reference_file2}: Different\n")
+                for attribute in set(ds_source.attrs).union(ds_target.attrs):
+                    if attribute not in ds_source.attrs or attribute not in ds_target.attrs or ds_source.attrs[attribute] != ds_target.attrs[attribute]:
+                        print(f"Global attribute '{attribute}' differs.")
+                        f.write(f"Global attribute '{attribute}' differs.\n")
+
+                for variable in set(ds_source.variables).union(ds_target.variables):
+                    if variable not in ds_source.variables or variable not in ds_target.variables:
+                        print(f"Variable '{variable}' is not present in both files.")
+                        f.write(f"Variable '{variable}' is not present in both files.\n")
+                    else:
+                        for attribute in set(ds_source[variable].attrs).union(ds_target[variable].attrs):
+                            if attribute not in ds_source[variable].attrs or attribute not in ds_target[variable].attrs or ds_source[variable].attrs[attribute] != ds_target[variable].attrs[attribute]:
+                                print(f"Attribute '{attribute}' of variable '{variable}' differs.")
+                                f.write(f"Attribute '{attribute}' of variable '{variable}' differs.\n")
+
+                        if not ds_source[variable].equals(ds_target[variable]):
+                            print(f"Data of variable '{variable}' differs.")
+                            f.write(f"Data of variable '{variable}' differs.\n")
+
+
+def main():
+
+    environment_name = "realcase_testing"
 
     with tempfile.TemporaryDirectory() as tmp:
 
-        if source_commit:
-            source_files_location = download_reference_files(source_commit_hash, output_directory=tmp, temp_folder_name="source_temp_repo")
-            source_references = create_reference_data(source_files_location, "source_data_references", tmp, "examples")
-        elif source == "HEAD":
-            source_files_location = get_head_notebooks(git_repo_path)
-            source_references = create_reference_data(source_files_location, "source_data_references", tmp, "examples")
+        if args.sv == "tmp":
+            save_directory = tmp
         else:
-            source_references = create_reference_data(git_repo_path, "source_data_references", tmp, "examples")
+            save_directory = args.sv
+        environment_path = os.path.join(save_directory, environment_name)
 
-        if target_commit:
-            target_files_location = download_reference_files(target_commit_hash, output_directory=tmp, temp_folder_name="target_temp_repo")
-            target_references = create_reference_data(target_files_location, "target_data_references", tmp, "examples")
-        elif target == "HEAD":
-            target_files_location = get_head_notebooks(git_repo_path)
-            target_references = create_reference_data(target_files_location, "target_data_references", tmp, "examples")
+        tobac_version = args.c1
+        if tobac_version.startswith("v"):
+            tobac_version = tobac_version[1::]
+
+        if args.c:
+            create_environment(environment_path, tobac_version)
         else:
-            target_references = create_reference_data(git_repo_path, "target_data_references", tmp, "examples")
+            subprocess.run(
+                ["mamba", "install", "-y", "-c", "conda-forge", "-p", environment_path, f"tobac={tobac_version}"], shell=True, check=True)
+        subprocess.run(["mamba", "run", "-p", environment_path, "python", "create_references.py", "--nb", args.nb, save_directory, "source_reference_data"], shell=True, check=True)
+        source_paths = get_reference_file_paths(os.path.join(save_directory, "source_reference_data"))
 
-        for source_reference in source_references:
-            target_reference = source_reference.replace("source_data_references", "target_data_references")
-            if os.path.exists(target_reference):
-                result = compare_files(source_reference, target_reference)
-                print(f"Comparison result for {source_reference} and {target_reference}: {'Same' if result else 'Different'}")
+        tobac_version = args.c2
+        if tobac_version.startswith("v"):
+            tobac_version = tobac_version[1::]
+        #environment_path = os.path.join(save_directory, environment_name
+
+        subprocess.run(["mamba", "install", "-y", "-c", "conda-forge", "-p", environment_path, f"tobac={tobac_version}"], shell=True, check=True)
+        subprocess.run(["mamba", "run", "-p", environment_path, "python", "create_references.py", "--nb", args.nb, save_directory, "target_reference_data"], shell=True, check=True)
+
+        for source_path in source_paths:
+            target_path = source_path.replace("source_reference_data", "target_reference_data")
+            if os.path.exists(target_path):
+                result = compare_files(source_path, target_path)
+                print(f"Comparison result for {source_path} and {target_path}: {'Same' if result else 'Different'}")
+                if not result:
+                    compare_files_detailed(source_path, target_path)
 
 
+#  python .\realcase_testing.py --nb v1.5.2 --c1 v1.5.2 --c2 v1.5.1 --sv "./testing" --c False
 if __name__ == "__main__":
     main()
